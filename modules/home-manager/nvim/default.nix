043b41aedbb95dd0eeffe2c76c6667dc0ba64caf
John Allen <john@threedogconsulting.com>
2024-01-23 13:57:26 -0500

    fix: add lagacy vim config

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -223,6 +223,7 @@
   in "${parsers}/parser";
 
   # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
-  xdg.configFile."nvim/lua".source = ./lua;
+  xdg.configFile."nvim/legacy.vim".source = ./legacy.vim;
   xdg.configFile."nvim/lazyvim.json".source = ./lazyvim.json;
+  xdg.configFile."nvim/lua".source = ./lua;
 }
