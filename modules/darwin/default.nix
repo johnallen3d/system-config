c6fc9773eb984e53ab0acc8af51f105e7433a153
John Allen <john@threedogconsulting.com>
2024-01-27 09:41:48 -0500

    feat: add garbage collection config

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -3,6 +3,16 @@
   services.nix-daemon.enable = true;
 
   nix = {
+    gc = {
+      automatic = true;
+      interval = {
+        Weekday = 0;
+        Hour = 0;
+        Minute = 0;
+      };
+      options = "--delete-older-than 30d";
+    };
+
     settings = {
       cores = 0; # max
       max-jobs = 10;
