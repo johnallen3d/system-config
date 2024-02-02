422de5cc7ff9af49a6d98ebb1c4bd8e0ad8e9306
John Allen <john@threedogconsulting.com>
2024-02-01 20:08:05 -0500

    feat: slightly better fish theme setup

--- modules/home-manager/fish/default.nix
+++ modules/home-manager/fish/default.nix
@@ -1,9 +1,24 @@
-{...}: {
+{pkgs, ...}: {
   programs.fish = {
     enable = true;
 
     interactiveShellInit = builtins.readFile ./config.fish;
+
+    plugins = [
+      {
+        name = "done";
+        src = pkgs.fishPlugins.done.src;
+      }
+      {
+        name = "colored-man-pages";
+        src = pkgs.fishPlugins.colored-man-pages.src;
+      }
+    ];
   };
+
+  # https://github.com/vitallium/tokyonight-fish
+  # https://github.com/nix-community/home-manager/issues/3724#issue-1604681266
+  home.file.".config/fish/themes/tokyo-night-moon.theme".source = ./tokyo-night-moon.theme;
 
   imports = [
     ./functions.nix
