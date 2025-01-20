5b4adaf0343b9704474e8f1fc420920b0fcdceb8
John Allen <john@threedogconsulting.com>
2025-01-20 10:35:06 -0500

    feat(nvim): tive `contextindent` a try
    This should apply correct indentation inside of codeblocks in markdown

--- modules/home-manager/nvim/lua/plugins/markdown.lua
+++ modules/home-manager/nvim/lua/plugins/markdown.lua
@@ -1,3 +1,11 @@
+return {
+	"wurli/contextindent.nvim",
+	-- This is the only config option; you can use it to restrict the files
+	-- which this plugin will affect (see :help autocommand-pattern).
+	opts = { pattern = "*" },
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+}
+
 -- return {
 -- 	"OXY2DEV/markview.nvim",
 -- 	lazy = false, -- Recommended
