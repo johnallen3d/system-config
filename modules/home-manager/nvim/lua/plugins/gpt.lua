d6421452c536bbc3694684074c9f0c60eb5071e3
John Allen <john@threedogconsulting.com>
2025-04-16 17:13:59 -0400

    feat(nvim): add `mcphub` support

--- modules/home-manager/nvim/lua/plugins/gpt.lua
+++ modules/home-manager/nvim/lua/plugins/gpt.lua
@@ -21,6 +21,16 @@ return {
 				chat = {
 					adapter = os.getenv("NVIM_CODECOMPANION_CHAT_ADAPTER")
 						or "copilot",
+					tools = {
+						["mcp"] = {
+							callback = function()
+								return require(
+									"mcphub.extensions.codecompanion"
+								)
+							end,
+							description = "Call tools and resources from the MCP Servers",
+						},
+					},
 				},
 				inline = {
 					adapter = "copilot",
@@ -43,5 +53,31 @@ return {
 				desc = "Toggle LLM Chat (CodeCompanion)",
 			},
 		},
+	},
+	{
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- comment the following line to ensure hub will be ready at the earliest
+		cmd = "MCPHub", -- lazy load by default
+		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+		-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+		config = function()
+			require("mcphub").setup({
+				auto_approve = true,
+				extensions = {
+					codecompanion = {
+						-- Show the mcp tool result in the chat buffer
+						show_result_in_chat = true,
+						-- Make chat #variables from MCP server resources
+						make_vars = true,
+						-- make /slash_commands from MCP server prompts
+						make_slash_commands = true,
+					},
+				},
+			})
+		end,
 	},
 }
