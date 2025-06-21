7d8181d36e890c4fec13bdce9b3291f1195d2ccc
John Allen <john@threedogconsulting.com>
2025-06-21 12:18:45 -0400

    feat(nvim): viva la `bullets.vim`!

--- modules/home-manager/nvim/lua/plugins/markdown.lua
+++ modules/home-manager/nvim/lua/plugins/markdown.lua
@@ -10,15 +10,13 @@ return {
 		},
 	},
 
-	-- TODO: check up on this issue
-	-- https://github.com/folke/snacks.nvim/issues/812
-	-- {
-	-- 	"bullets-vim/bullets.vim",
-	-- 	ft = { "markdown" },
-	-- 	config = function()
-	-- 		vim.g.bullets_delete_last_bullet_if_empty = 1
-	-- 	end,
-	-- },
+	{
+		"bullets-vim/bullets.vim",
+		ft = { "markdown" },
+		config = function()
+			vim.g.bullets_delete_last_bullet_if_empty = 1
+		end,
+	},
 
 	{
 		"mfussenegger/nvim-lint",
