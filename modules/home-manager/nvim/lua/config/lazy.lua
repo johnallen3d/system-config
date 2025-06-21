f9305b6baacd257a24b75e9854e63f88246c68f0
John Allen <john@threedogconsulting.com>
2025-06-21 12:18:21 -0400

    feat(nvim): add rest client support

--- modules/home-manager/nvim/lua/config/lazy.lua
+++ modules/home-manager/nvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.test.core" },
 		{ import = "lazyvim.plugins.extras.util.dot" },
+		{ import = "lazyvim.plugins.extras.util.rest" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },
