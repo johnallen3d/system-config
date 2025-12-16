f78e6b63c6939f32ff80a6ecb4f2b59e411e6d88
John Allen <john@threedogconsulting.com>
2025-12-16 17:18:04 -0500

    feat(packages): add home assistant desktop app

--- modules/darwin/homebrew/mas.nix
+++ modules/darwin/homebrew/mas.nix
@@ -4,6 +4,7 @@
       "1Password for Safari" = 1569813296;
       "Balance Lock" = 1019371109;
       "CARROT Weather" = 993487541;
+      "Home Assistant" = 1099568401;
       "Key Codes" = 414568915;
       "Pixelmator Pro" = 1289583905;
       "Slack" = 803453959;
