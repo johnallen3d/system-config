0d30f57eeef3e75151969c747995244920fdad72
John Allen <john@threedogconsulting.com>
2025-12-17 10:13:35 -0500

    chore(packages): move back to unstable
    looks like `flyctl` issue is resolved

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   description = "nix-darwin and home-manager configuration";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # Temporarily using master to get flyctl hash fix (PR #461191)
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
