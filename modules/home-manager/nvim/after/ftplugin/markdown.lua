030b0d8eedfd6d7f8ef0e9e6063b7b85d07f9c74
John Allen <john@threedogconsulting.com>
2026-02-28 13:57:49 -0500

    chore(nvim): move from Marked to Meva for markdown preview

--- modules/home-manager/nvim/after/ftplugin/markdown.lua
+++ modules/home-manager/nvim/after/ftplugin/markdown.lua
@@ -3,7 +3,7 @@ vim.opt_local.spell = true
 
 if vim.fn.has("mac") == 1 then
   vim.keymap.set("n", "<Leader>m", function()
-    vim.cmd('silent !open -a Marked\\ 2.app "' .. vim.fn.expand("%:p") .. '"')
+    vim.cmd('silent !open -a Meva "' .. vim.fn.expand("%:p") .. '"')
   end, { buffer = true, desc = "Open Marked 2 preview" })
 end
 
