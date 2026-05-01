e933946d3f2ef89482f536ffc43008e0f808f49b
John Allen <john@threedogconsulting.com>
2026-05-01 14:10:57 -0400

    chore(beads): close implement prompt task

--- modules/home-manager/pi-prompts.nix
+++ modules/home-manager/pi-prompts.nix
@@ -1,10 +1,15 @@
 {lib, ...}: {
-  home.file.".config/pi/prompts" = {
-    recursive = true;
-    source = ./pi-prompts;
-  };
+  home.file.".config/pi/prompts/pkg-install.md".source = ./pi-prompts/pkg-install.md;
+  home.file.".config/pi/prompts/wrap.md".source = ./pi-prompts/wrap.md;
+  home.file.".config/pi-work/prompts/implement.md".source = ./pi-prompts/implement.md;
 
-  home.activation.piWorkPromptCleanup = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    rm -rf "$HOME/.config/pi-work/prompts"
+  home.activation.piPromptDirMigration = lib.hm.dag.entryBefore ["writeBoundary"] ''
+    if [ -L "$HOME/.config/pi/prompts" ]; then
+      rm -f "$HOME/.config/pi/prompts"
+    fi
+
+    if [ -L "$HOME/.config/pi-work/prompts" ]; then
+      rm -f "$HOME/.config/pi-work/prompts"
+    fi
   '';
 }
