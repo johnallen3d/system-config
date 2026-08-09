7244842ef66977ae3e93a979f5b0c6992a1923de
John Allen <john@threedogconsulting.com>
2026-08-09 13:58:47 -0400

    chore(packages): remove carrot
    - it's failing to install
    - never really worked correctly 😢

--- modules/darwin/homebrew/mas.nix
+++ modules/darwin/homebrew/mas.nix
@@ -3,7 +3,7 @@
     masApps = {
       "1Password for Safari" = 1569813296;
       "Balance Lock" = 1019371109;
-      "CARROT Weather" = 993487541;
+      # "CARROT Weather" = 993487541;
       "Home Assistant" = 1099568401;
       "Infuse" = 1136220934;
       "Key Codes" = 414568915;
